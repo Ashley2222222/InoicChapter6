597b90661a89e37db3ab03bcce8bfc2511860caf
YolandaLeung <tot.123@163.com>
2017-08-28 17:16:48 +0800

    step 5 finished

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,91 @@
-// Ionic Starter App
+angular.module('App', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('search', {
+      url: '/search',
+      controller: 'SearchController',
+      templateUrl: 'views/search/search.html'
+    })
+    .state('settings', {
+      url: '/settings',
+      controller: 'SettingsController',
+      templateUrl: 'views/settings/settings.html'
+    })
+    .state('weather',{
+      url:'/weather/:city/:lat/:lng',
+      controller:'WeatherController',
+      templateUrl:'view/weather/weather.html'
+    });
+
+  $urlRouterProvider.otherwise('/search');
+})
+.controller('LeftMenuController',function($scope,Locations){
+  $scope.locations=Locations.data;
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
     }
-    if (window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
+    if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
+})
+
+.controller('LeftMenuController', function ($scope, Locations) {
+  $scope.locations = Locations.data;
+})
+
+.factory('Settings', function () {
+  var Settings = {
+    units: 'us',
+    days: 8
+  };
+  return Settings;
+})
+
+.factory('Locations', function () {
+  var Locations = {
+    data: [{
+      city: 'Chicago, IL, USA',
+      lat: 41.8781136,
+      lng: -87.6297982
+    },{
+      city: 'Canton, GD, CHN',
+      lat: 23.20,
+      lng: 113.30
+    }],
+    getIndex: function (item) {
+      var index = -1;
+      angular.forEach(Locations.data, function (location, i) {
+        if (item.lat == location.lat && item.lng == location.lng) {
+          index = i;
+        }
+      });
+      return index;
+    },
+    toggle: function (item) {
+      var index = Locations.getIndex(item);
+      if (index >= 0) {
+        Locations.data.splice(index, 1);
+      } else {
+        Locations.data.push(item);
+      }
+    },
+    primary: function (item) {
+      var index = Locations.getIndex(item);
+      if (index >= 0) {
+        Locations.data.splice(index, 1);
+        Locations.data.splice(0, 0, item);
+      } else {
+        Locations.data.unshift(item);
+      }
+    }
+  };
+
+  return Locations;
 });
